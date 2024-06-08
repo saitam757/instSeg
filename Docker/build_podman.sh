ce4239e2c98738ea891f8094f351bfc8a4cca7ee
bender <bender@local.org>
2024-06-08 19:48:52 +0200

    cleanup dockerfiles

--- Docker/build_podman.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-                                           
-#cp ../requirements.txt .
-
-podman build --rm --no-cache --file Dockerfile --tag tensorflow:1.0 . 
--- Docker/run_podman.sh
+++ Docker/run_podman.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-XAUTH=$(realpath /tmp/xauth*)
-echo $XAUTH
+#XAUTH=$(realpath /tmp/xauth*)
+#echo $XAUTH
 echo $HOME
 
 HOST_WORKSPACE="$HOME/Development/Python/instSeg"
@@ -15,12 +15,13 @@ podman run --rm --network host -it \
 --security-opt=label=disable \
 --userns=keep-id \
 --env DISPLAY=$DISPLAY \
---env="QT_X11_NO_MITSHM=1" \
---volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
---volume="$XAUTH:/home/project/.Xauthority:z" \
 --mount source="$HOST_WORKSPACE",target=$DOCKER_WORKSPACE,type=bind,consistency=cached \
 --workdir $DOCKER_WORKSPACE \
 --env HOME=$DOCKER_WORKSPACE \
 --env PYTHONPATH=$DOCKER_WORKSPACE \
 tensorflow:1.0
+
+#--env="QT_X11_NO_MITSHM=1" \
+#--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+#--volume="$XAUTH:/home/project/.Xauthority:z" \
 
